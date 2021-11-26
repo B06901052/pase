f0e3a164857c904a6575557c10f2c782169a8189
Joseph Feng <b06901052@ntu.edu.tw>
2021-11-26 14:52:07 +0800

    fix bug in path

--- data/prep/prepare_openslr_rirs_cfg.py
+++ data/prep/prepare_openslr_rirs_cfg.py
@@ -6,8 +6,9 @@ import numpy as np
 import os
 import re
 
+
 def load_filenames(opts):
-    rooms = {'smallroom':[], 'mediumroom':[], 'largeroom':[]}
+    rooms = {'smallroom': [], 'mediumroom': [], 'largeroom': []}
     for room in rooms.keys():
         rir_list_fn = os.path.join(opts.data_root, room, 'rir_list')
         with open(rir_list_fn, 'r') as fn:
@@ -15,27 +16,29 @@ def load_filenames(opts):
                 rooms[room].append(line.split(' ')[4].strip())
     return rooms
 
+
 def main(opts):
 
     if opts.existing_cfg is not None:
         with open(opts.existing_cfg, 'r') as ex_f:
             out = json.load(ex_f)
-            out['reverb_data_root'] = opts.data_root
+            out['reverb_data_root'] = os.path.abspath(
+                os.path.join(opts.data_root, os.pardir))
             out['reverb_fmt'] = 'wav'
             out['reverb_irfiles'] = []
     else:
-        out = {"reverb_data_root":opts.data_root, 
-               "reverb_fmt":"wav",
-               "reverb_irfiles":[]}
+        out = {"reverb_data_root": opts.data_root,
+               "reverb_fmt": "wav",
+               "reverb_irfiles": []}
 
     rooms = load_filenames(opts)
     final_rirs = []
 
     rirs = rooms['smallroom']
     if opts.small_room_ratio < 1.0:
-        sel = int(len(rirs)*opts.small_room_ratio)
-        print ('Found {} in small room. Selecting random {} out of them.'\
-                .format(len(rirs), sel))
+        sel = int(len(rirs) * opts.small_room_ratio)
+        print('Found {} in small room. Selecting random {} out of them.'
+              .format(len(rirs), sel))
         shuffle(rirs)
         final_rirs.extend(rirs[:sel])
     else:
@@ -43,9 +46,9 @@ def main(opts):
 
     rirs = rooms['mediumroom']
     if opts.medium_room_ratio < 1.0:
-        sel = int(len(rirs)*opts.medium_room_ratio)
-        print ('Found {} in medium room. Selecting random {} out of them.'\
-                .format(len(rirs), sel))
+        sel = int(len(rirs) * opts.medium_room_ratio)
+        print('Found {} in medium room. Selecting random {} out of them.'
+              .format(len(rirs), sel))
         shuffle(rirs)
         final_rirs.extend(rirs[:sel])
     else:
@@ -53,19 +56,19 @@ def main(opts):
 
     rirs = rooms['largeroom']
     if opts.large_room_ratio < 1.0:
-        sel = int(len(rirs)*opts.large_room_ratio)
-        print ('Found {} in large room. Selecting random {} out of them.'\
-                .format(len(rirs), sel))
+        sel = int(len(rirs) * opts.large_room_ratio)
+        print('Found {} in large room. Selecting random {} out of them.'
+              .format(len(rirs), sel))
         shuffle(rirs)
         final_rirs.extend(rirs[:sel])
     else:
         final_rirs.extend(rirs)
 
-    print ('Found total {} rir paths'.format(len(final_rirs)))
+    print('Found total {} rir paths'.format(len(final_rirs)))
     out["reverb_irfiles"].extend(sorted(final_rirs))
 
     with open(opts.out_file, 'w') as f:
-        f.write(json.dumps(out, indent=2))
+        f.write(json.dumps(out, indent=0))
 
 
 if __name__ == '__main__':
@@ -80,7 +83,5 @@ if __name__ == '__main__':
     parser.add_argument('--existing_cfg', type=str, default=None)
     parser.add_argument('--out_file', type=str, required=True)
 
-    
     opts = parser.parse_args()
     main(opts)
-
